c5a4b19d26bba98811dbfb0fdb9c81ff83deb1c1
abhijeet8900 <abhijeet8900@gmail.com>
2023-09-24 19:08:23 +0530

    - Add wordpress logo in page header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const Header = () => {
   return (
-    <a href="https://central.wordcamp.org/">
-      <h1 className="w-full mx-auto text-primary font-bold text-center text-6xl my-4 leading-normal">
+    <a href="https://central.wordcamp.org/" className="no-underline flex">
+      <h1 className="w-full flex justify-center items-center text-primary font-bold text-center text-6xl my-4 leading-normal">
+        <Icon
+          icon="uil:wordpress"
+          style={{ fontSize: "72px", marginRight: "16px" }}
+        />{" "}
         WordCamp Viewer
       </h1>
     </a>
