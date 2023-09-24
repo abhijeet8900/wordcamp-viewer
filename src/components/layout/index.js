78dbd4c23ebda91ad19a8da84e710c08340ee607
abhijeet8900 <abhijeet8900@gmail.com>
2023-09-24 18:58:47 +0530

    - Update layout component styles

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Header from '../header'
+import React from "react";
+import Header from "../header";
 
-const Layout = ({ children}) => {
+const Layout = ({ children }) => {
   return (
-    <div className='w-full h-full max-w-5xl mx-auto'>
-    <Header />
-    {children}
+    <div className="w-full h-full max-w-6xl mx-auto">
+      <Header />
+      <div className="flex gap-x-2 max-h-[720px]">{children}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
